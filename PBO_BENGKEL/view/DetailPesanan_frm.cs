d3216dab3fc962e8499e05964ed5fff15283f19c
arum <arumsetia58@gmail.com>
2026-05-30 19:20:58 +0700

    Save Form Mekanik

---
+++ PBO_BENGKEL/view/DetailPesanan_frm.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using PBO_BENGKEL.konfigurasi;
+
+namespace PBO_BENGKEL.view
+{
+    public partial class DetailPesanan_frm : Form
+    {
+        private string idServis;
+        public DetailPesanan_frm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public DetailPesanan_frm(string idPelanggan, string namaPelanggan, string keluhan)
+        {
+            InitializeComponent();
+            id_txt.Text = idPelanggan;
+            namapelanggan_txt.Text = namaPelanggan;
+            keluhan_txt.Text = keluhan;
+
+            LoadJasa();
+            LoadSparepart();
+            LoadStatus();
+        }
+
+        private void LoadJasa()
+        {
+            try
+            {
+                using (MySqlConnection conn = Koneksi.GetConn())
+                {
+                    conn.Open();
+
+                    string query =
+                        "SELECT id_jasa, nama_jasa FROM tabel_jasa";
+
+                    MySqlDataAdapter da =
+                        new MySqlDataAdapter(query, conn);
+
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    jasa_cmb.DataSource = dt;
+                    jasa_cmb.DisplayMember = "nama_jasa";
+                    jasa_cmb.ValueMember = "id_jasa";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Load Jasa : " + ex.Message);
+            }
+        }
+
+        private void LoadSparepart()
+        {
+            try
+            {
+                using (MySqlConnection conn = Koneksi.GetConn())
+                {
+                    conn.Open();
+
+                    string query =
+                        "SELECT id_sparepart, nama_sparepart FROM tabel_sparepart";
+
+                    MySqlDataAdapter da =
+                        new MySqlDataAdapter(query, conn);
+
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    sparepart_cmb.DataSource = dt;
+                    sparepart_cmb.DisplayMember = "nama_sparepart";
+                    sparepart_cmb.ValueMember = "id_sparepart";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Load Sparepart : " + ex.Message);
+            }
+        }
+
+        private void LoadStatus()
+        {
+            status_cmb.Items.Clear();
+
+            status_cmb.Items.Add("Selesai");
+
+            status_cmb.SelectedIndex = 0;
+        }
+
+        private void selesai_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection conn = Koneksi.GetConn())
+                {
+                    conn.Open();
+
+                    string query = @"
+            INSERT INTO tabel_servis_selesai
+            (
+                id_pelanggan,
+                nama_pelanggan,
+                keluhan,
+                id_jasa,
+                id_sparepart,
+                status
+            )
+            VALUES
+            (
+                @id,
+                @nama,
+                @keluhan,
+                @jasa,
+                @sparepart,
+                @status
+            )";
+
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                    cmd.Parameters.AddWithValue("@id", id_txt.Text);
+                    cmd.Parameters.AddWithValue("@nama", namapelanggan_txt.Text);
+                    cmd.Parameters.AddWithValue("@keluhan", keluhan_txt.Text);
+                    cmd.Parameters.AddWithValue("@jasa", jasa_cmb.SelectedValue);
+                    cmd.Parameters.AddWithValue("@sparepart", sparepart_cmb.SelectedValue);
+                    cmd.Parameters.AddWithValue("@status", status_cmb.Text);
+
+                    cmd.ExecuteNonQuery();
+
+                    string deletePesanan =
+                        "DELETE FROM tabel_pesanan WHERE nama_pelanggan=@nama";
+
+                    MySqlCommand cmdPesanan =
+                        new MySqlCommand(deletePesanan, conn);
+
+                    cmdPesanan.Parameters.AddWithValue(
+                        "@nama",
+                        namapelanggan_txt.Text);
+
+                    cmdPesanan.ExecuteNonQuery();
+
+                    string deletePelanggan =
+                        "DELETE FROM tabel_pelanggan WHERE id_pelanggan=@id";
+
+                    MySqlCommand cmdPelanggan =
+                        new MySqlCommand(deletePelanggan, conn);
+
+                    cmdPelanggan.Parameters.AddWithValue(
+                        "@id",
+                        id_txt.Text);
+
+                    cmdPelanggan.ExecuteNonQuery();
+
+                    MessageBox.Show("Pesanan berhasil diselesaikan");
+
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    //ex.Message
+                    ex.ToString(),
+                    "ERROR",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void tutup_btn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- PBO_BENGKEL/view/MekanikForm.Designer.cs
+++ PBO_BENGKEL/view/MekanikForm.Designer.cs
@@ -28,12 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.namamekanik_cmb = new System.Windows.Forms.ComboBox();
+            this.antrian_dgv = new System.Windows.Forms.DataGridView();
+            this.detail_btn = new System.Windows.Forms.Button();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nama = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Plat = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Keluhan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.antrian_dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(438, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ANTRIAN PESANAN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(106, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama Mekanik :";
+            // 
+            // namamekanik_cmb
+            // 
+            this.namamekanik_cmb.FormattingEnabled = true;
+            this.namamekanik_cmb.Location = new System.Drawing.Point(253, 108);
+            this.namamekanik_cmb.Name = "namamekanik_cmb";
+            this.namamekanik_cmb.Size = new System.Drawing.Size(375, 24);
+            this.namamekanik_cmb.TabIndex = 3;
+            // 
+            // antrian_dgv
+            // 
+            this.antrian_dgv.AllowUserToAddRows = false;
+            this.antrian_dgv.AllowUserToDeleteRows = false;
+            this.antrian_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.antrian_dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Nama,
+            this.Plat,
+            this.Keluhan});
+            this.antrian_dgv.Location = new System.Drawing.Point(110, 148);
+            this.antrian_dgv.Name = "antrian_dgv";
+            this.antrian_dgv.RowHeadersWidth = 51;
+            this.antrian_dgv.RowTemplate.Height = 24;
+            this.antrian_dgv.Size = new System.Drawing.Size(800, 426);
+            this.antrian_dgv.TabIndex = 4;
+            // 
+            // detail_btn
+            // 
+            this.detail_btn.Location = new System.Drawing.Point(683, 580);
+            this.detail_btn.Name = "detail_btn";
+            this.detail_btn.Size = new System.Drawing.Size(227, 48);
+            this.detail_btn.TabIndex = 6;
+            this.detail_btn.Text = "DETAIL";
+            this.detail_btn.UseVisualStyleBackColor = true;
+            this.detail_btn.Click += new System.EventHandler(this.detail_btn_Click);
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "id_pelanggan";
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 6;
+            this.ID.Name = "ID";
+            this.ID.Width = 125;
+            // 
+            // Nama
+            // 
+            this.Nama.DataPropertyName = "nama_pelanggan";
+            this.Nama.HeaderText = "Nama";
+            this.Nama.MinimumWidth = 6;
+            this.Nama.Name = "Nama";
+            this.Nama.Width = 200;
+            // 
+            // Plat
+            // 
+            this.Plat.DataPropertyName = "plat_motor";
+            this.Plat.HeaderText = "Plat Motor";
+            this.Plat.MinimumWidth = 6;
+            this.Plat.Name = "Plat";
+            this.Plat.Width = 125;
+            // 
+            // Keluhan
+            // 
+            this.Keluhan.DataPropertyName = "keluhan";
+            this.Keluhan.HeaderText = "Keluhan";
+            this.Keluhan.MinimumWidth = 6;
+            this.Keluhan.Name = "Keluhan";
+            this.Keluhan.Width = 300;
+            // 
+            // AntrianPesanan_frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MekanikForm";
+            this.ClientSize = new System.Drawing.Size(1031, 699);
+            this.Controls.Add(this.detail_btn);
+            this.Controls.Add(this.antrian_dgv);
+            this.Controls.Add(this.namamekanik_cmb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AntrianPesanan_frm";
+            this.Text = "AntrianPesanan_frm";
+            ((System.ComponentModel.ISupportInitialize)(this.antrian_dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox namamekanik_cmb;
+        private System.Windows.Forms.DataGridView antrian_dgv;
+        private System.Windows.Forms.Button detail_btn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nama;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Plat;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Keluhan;
     }
 }
